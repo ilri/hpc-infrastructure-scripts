d536d9eca45957c920431203abe058f069a32b2e
Alan Orth <alan.orth@gmail.com>
2013-05-31 11:44:59 +0300

    389_useradd.sh: Style and formatting cleanups

--- 389_useradd.sh
+++ 389_useradd.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
+
 function Usage {
-echo "Usage: $0 -f FirstName -i UserID -l LastName -u UserName [ -s Shell -g GroupID -p password]"
-exit 1
+    echo "Usage: $0 -f FirstName -i UserID -l LastName -u UserName [ -s shell -g GroupID -p password]"
+    exit 1
 }
-Shell=/bin/bash
+
+SHELL=/bin/bash
 GroupID=100
 Password=redhat
+
 while getopts f:g:i:l:i:p:u: OPTION
 do
-case $OPTION in
-f) FirstName=$OPTARG;;
-g) GroupID=$OPTARG;;
-i) UserID=$OPTARG;;
-l) LastName=$OPTARG;;
-p) Password=$OPTARG;;
-u) UserName=$OPTARG;;
-esac
+    case $OPTION in
+        f) FirstName=$OPTARG;;
+        g) GroupID=$OPTARG;;
+        i) UserID=$OPTARG;;
+        l) LastName=$OPTARG;;
+        p) Password=$OPTARG;;
+        u) UserName=$OPTARG;;
+    esac
 done
+
 if [ "$FirstName" == "" ]; then Usage; fi
 if [ "$LastName" == "" ]; then Usage; fi
 if [ "$UserID" == "" ]; then Usage; fi
 if [ "$UserName" == "" ]; then
-FirstInitial=`echo $FirstName | cut -c1`
-UserName=`echo "${FirstInitial}${LastName}" | tr "[:upper:]" "[:lower:]"`
+    FirstInitial=`echo $FirstName | cut -c1`
+    UserName=`echo "${FirstInitial}${LastName}" | tr "[:upper:]" "[:lower:]"`
 fi
 
 echo "dn: uid=$UserName, ou=People, dc=theyjas,dc=com"
@@ -39,6 +43,6 @@ echo "givenName: $FirstName $LastName"
 echo "gidNumber: $GroupID"
 echo "uidNumber: $UserID"
 echo "userPassword: {clear}$Password"
-echo "loginShell: $Shell"
+echo "loginShell: $SHELL"
 echo "homeDirectory: /home/$UserName"
 echo
