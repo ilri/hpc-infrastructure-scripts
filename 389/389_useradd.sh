b8b7dd7284bbbe6db37440343b9a4bb74e4d5a77
Alan Orth <a.orth@cgiar.org>
2014-07-30 12:02:55 +0300

    389/389_useradd.sh: Switch to functional programming
    - Add functions for logical functionality
    - Use function-local variables (lowercase)
    - Use global readonly defaults (UPPERCASE)
    
    Still need to clean up some other variable names and keep compartm-
    entalizing functionality.

--- 389/389_useradd.sh
+++ 389/389_useradd.sh
@@ -24,6 +24,9 @@ readonly PROGNAME=$(basename $0)
 readonly PROGDIR=$(readlink -m $(dirname $0))
 readonly ARGS="$@"
 
+readonly DEF_SHELL=/bin/bash
+readonly DEF_PASSWORD=redhat
+
 function usage {
     cat <<- EOF
 Usage: $PROGNAME -f FirstName -l LastName [ -u username -i userid -g groupid -p password]
@@ -32,76 +35,94 @@ Optional arguments:
     -u: username
     -i: numeric userid (default: latest available)
     -g: numeric groupid (default: latest available)
-    -p: password (default: $Password)
+    -p: password (default: $DEF_PASSWORD)
 EOF
 
     exit 0
 }
 
-SHELL=/bin/bash
-Password=redhat
+function parse_arguments {
+    while getopts f:g:i:l:i:p:u:h OPTION
+    do
+        case $OPTION in
+            f) FirstName=$OPTARG;;
+            g) GroupID=$OPTARG;;
+            i) UserID=$OPTARG;;
+            l) LastName=$OPTARG;;
+            p) Password=$OPTARG;;
+            u) UserName=$OPTARG;;
+            h) usage;;
+        esac
+    done
+}
 
-while getopts f:g:i:l:i:p:u:h OPTION
-do
-    case $OPTION in
-        f) FirstName=$OPTARG;;
-        g) GroupID=$OPTARG;;
-        i) UserID=$OPTARG;;
-        l) LastName=$OPTARG;;
-        p) Password=$OPTARG;;
-        u) UserName=$OPTARG;;
-        h) usage;;
-    esac
-done
+function generate_ldif {
 
-[[ -z "$FirstName" ]] && usage
-[[ -z "$LastName" ]] && usage
-if [[ -z "$UserID" ]]; then
-    LatestUID=`ldapsearch -x "objectclass=posixAccount" uidNumber | grep -v \^dn | grep -v \^\$ | sed -e 's/uidNumber: //g' | grep -E "^[0-9]{3,4}$" | sort -n | tail -n 1`
-    UserID=$((LatestUID + 1))
-fi
-if [[ -z "$GroupID" ]]; then
-    LatestGID=`ldapsearch -x "objectclass=posixGroup" gidNumber | grep -v \^dn | grep -v \^\$ | sed -e 's/gidNumber: //g' | grep -E "^[0-9]{3,4}$" | sort -n | tail -n 1`
-    GroupID=$((LatestGID + 1))
-fi
-if [[ -z "$UserName" ]]; then
-    FirstInitial=`echo $FirstName | cut -c1`
-    UserName=`echo "${FirstInitial}${LastName}" | tr "[:upper:]" "[:lower:]"`
-fi
+    # check existence and validity of parameters
+    [[ -z "$FirstName" ]] && usage
+    [[ -z "$LastName" ]] && usage
+    if [[ -z "$UserID" ]]; then
+        LatestUID=`ldapsearch -x "objectclass=posixAccount" uidNumber | grep -v \^dn | grep -v \^\$ | sed -e 's/uidNumber: //g' | grep -E "^[0-9]{3,4}$" | sort -n | tail -n 1`
+        UserID=$((LatestUID + 1))
+    fi
+    if [[ -z "$GroupID" ]]; then
+        LatestGID=`ldapsearch -x "objectclass=posixGroup" gidNumber | grep -v \^dn | grep -v \^\$ | sed -e 's/gidNumber: //g' | grep -E "^[0-9]{3,4}$" | sort -n | tail -n 1`
+        GroupID=$((LatestGID + 1))
+    fi
+    if [[ -z "$UserName" ]]; then
+        FirstInitial=`echo $FirstName | cut -c1`
+        UserName=`echo "${FirstInitial}${LastName}" | tr "[:upper:]" "[:lower:]"`
+    fi
 
-# Print LDIF for user account
-printf "dn: uid=%s, ou=People, dc=ilri,dc=cgiar,dc=org\n" "$UserName"
-printf "changetype: add\n"
-printf "givenName: %s\n" "$FirstName"
-printf "sn: %s\n" "$LastName"
-printf "loginShell: %s\n" "$SHELL"
-printf "gidNumber: %d\n" "$GroupID"
-printf "uidNumber: %d\n" "$UserID"
-printf "objectClass: top\n"
-printf "objectClass: person\n"
-printf "objectClass: organizationalPerson\n"
-printf "objectClass: inetorgperson\n"
-printf "objectClass: posixAccount\n"
-printf "uid: %s\n" "$UserName"
-printf "gecos: %s %s\n" "$FirstName" "$LastName"
-printf "cn: %s %s\n" "$FirstName" "$LastName"
-# send password in clear text, so 389 can hash using the best scheme
-# see: https://lists.fedoraproject.org/pipermail/389-users/2012-August/014908.html
-printf "userPassword: %s\n" "$Password"
-printf "homeDirectory: /home/%s\n\n" "$UserName"
+    local username=$UserName
+    local firstname=$FirstName
+    local lastname=$LastName
+    local shell=$SHELL
+    local groupid=$GroupID
+    local userid=$UserID
+    local password=${Password:-$DEF_PASSWORD}
 
-# Print LDIF for primary group
-printf "dn: cn=%s, ou=Groups, dc=ilri,dc=cgiar,dc=org\n" "$UserName"
-printf "changetype: add\n"
-printf "gidNumber: %d\n" "$GroupID"
-printf "memberUid: %s\n" "$UserName"
-printf "objectClass: top\n"
-printf "objectClass: groupofuniquenames\n"
-printf "objectClass: posixgroup\n"
-printf "cn: %s\n\n" "$UserName"
+    # Print LDIF for user account
+    printf "dn: uid=%s, ou=People, dc=ilri,dc=cgiar,dc=org\n" "$username"
+    printf "changetype: add\n"
+    printf "givenName: %s\n" "$firstname"
+    printf "sn: %s\n" "$lastname"
+    printf "loginShell: %s\n" "$shell"
+    printf "gidNumber: %d\n" "$groupid"
+    printf "uidNumber: %d\n" "$userid"
+    printf "objectClass: top\n"
+    printf "objectClass: person\n"
+    printf "objectClass: organizationalPerson\n"
+    printf "objectClass: inetorgperson\n"
+    printf "objectClass: posixAccount\n"
+    printf "uid: %s\n" "$username"
+    printf "gecos: %s %s\n" "$firstname" "$lastname"
+    printf "cn: %s %s\n" "$firstname" "$lastname"
+    # send password in clear text, so 389 can hash using the best scheme
+    # see: https://lists.fedoraproject.org/pipermail/389-users/2012-August/014908.html
+    printf "userPassword: %s\n" "$password"
+    printf "homeDirectory: /home/%s\n\n" "$username"
 
-# add user to SSH group
-printf "dn: cn=ssh, ou=Groups, dc=ilri,dc=cgiar,dc=org\n"
-printf "changetype: modify\n"
-printf "add: memberuid\n"
-printf "memberuid: %s\n" "$UserName"
+    # Print LDIF for primary group
+    printf "dn: cn=%s, ou=Groups, dc=ilri,dc=cgiar,dc=org\n" "$username"
+    printf "changetype: add\n"
+    printf "gidNumber: %d\n" "$groupid"
+    printf "memberUid: %s\n" "$username"
+    printf "objectClass: top\n"
+    printf "objectClass: groupofuniquenames\n"
+    printf "objectClass: posixgroup\n"
+    printf "cn: %s\n\n" "$username"
+
+    # add user to SSH group
+    printf "dn: cn=ssh, ou=Groups, dc=ilri,dc=cgiar,dc=org\n"
+    printf "changetype: modify\n"
+    printf "add: memberuid\n"
+    printf "memberuid: %s\n" "$username"
+}
+
+function main {
+    parse_arguments $ARGS
+    generate_ldif
+}
+
+main
