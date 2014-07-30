4ee684ee0a0f8953976bf1a3cbac0d1a1621887d
Alan Orth <a.orth@cgiar.org>
2014-07-30 10:44:24 +0300

    389/389_useradd.sh: Update copyright to 2014

--- 389/389_useradd.sh
+++ 389/389_useradd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (C) 2013 Alan Orth
+# Copyright (C) 2014 Alan Orth
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
